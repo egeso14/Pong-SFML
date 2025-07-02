1f330b68ae5cc5079d03618b869520a1adfef21b
egeso14 <egeyilmaz2023@u.northwestern.edu>
2025-07-01 19:56:07 -0500

    added event polling. The window can be closed now.

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include "Header/Core/GameWindowManager.h"
+#include "Header/Event/EventManager.h"
 
 int main()
 {
 	Core::GameWindowManager gameWindowManager;
+	Events::EventManager eventManager;
 	gameWindowManager.initialize();
 	
 	while (gameWindowManager.isGameRunning())
 	{
+		eventManager.pollEvents(gameWindowManager.getGameWindow());
 		gameWindowManager.render();
 	}
 
